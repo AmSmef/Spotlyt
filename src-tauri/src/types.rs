ea11748fa4920724c81b6d4e97a021eec29850bd
Adam47 <amsmef@gmail.com>
2026-03-20 19:36:50 +0000

    Basic string normalisation fucntion for artist names between s and tm

--- src-tauri/src/types.rs
+++ src-tauri/src/types.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use unidecode::unidecode;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Artist {
@@ -13,4 +14,18 @@ pub struct Concert {
     pub venue: String,
     pub city: String,
     pub date: String,
+}
+
+// normalises every artist name to be lowercase, without accents and without punctuation
+// this is used when comparing artist names between spotify and ticketmaster, as there's a chance they may be stored differently
+// like "AC/DC" vs "ACDC", or "Fred Again.." vs "Fred Again"
+pub fn normalise_artist_name(name: &str) -> String {
+    unidecode(name)
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphanumeric() || c.is_whitespace())
+        .collect::<String>()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join(" ")
 }
